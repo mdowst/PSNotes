11fe4598afaf061d038ad4250574a6e88175a28b
mdowst <7384306+mdowst@users.noreply.github.com>
2026-02-08 08:11:41 -0500

    update tests

---
+++ tools/tests-build.ps1
@@ -0,0 +1,26 @@
+$Parent = $PSScriptRoot
+while ( -not (Test-Path (Join-Path $Parent 'src'))) {
+    $Parent = Split-Path $Parent -Parent
+}
+
+$TestPath = Join-Path $Parent 'tests'
+$binPath = Join-Path $Parent 'bin'
+if(-not (Test-Path $binPath)){
+   New-Item -ItemType Directory -Force -Path $binPath | Out-Null
+}
+
+$psd1 = Get-ChildItem -Path $binPath -Recurse -Filter 'PSNotes.psd1' | Select-Object -Last 1 -ExpandProperty FullName
+if(-not $psd1){
+    Write-Host "PSNotes.psd1 not found in $binPath. Please build the module before running tests."
+    exit 1
+}
+
+# Run Unit Tests
+$config = New-PesterConfiguration
+$config.Output.Verbosity = 'Detailed'
+$config.Run.Throw = $false
+$config.TestResult.Enabled = $true
+$config.TestResult.OutputFormat = 'JUnitXml'
+$config.Run.Path = (Join-Path $TestPath 'Build')
+$config.TestResult.OutputPath   = (Join-Path $binPath 'Build.TestResults.xml')
+Invoke-Pester -Configuration $config
